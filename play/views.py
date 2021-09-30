ca45d52d39703a9636b7388a672d67c73f73786a
Andy Kuck <andrew.kuck@valpo.edu>
2021-09-29 23:08:50 -0400

    Have JS version of puzzle gen fully working

--- play/views.py
+++ play/views.py
@@ -4,20 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     
-    gridlist = [
-                    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-                    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-                    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-                    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-                    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-                    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-                    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-                    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-                ]
-                
-    size = str(100/len(gridlist)) + '%'
     
     return render(request, 'play/index.html', {
-        'gridlist': gridlist,
-        'size': size,
+    
     })
