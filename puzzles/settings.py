26957e394ee7929a58545d60379b94ba06d6d3a8
Andy Kuck <andrew.kuck@valpo.edu>
2023-02-23 11:28:21 -0500

    Fixed some security concerns

--- puzzles/settings.py
+++ puzzles/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lt_on-)(qf99b-cew181kcpmva#f7!zlh_nx5vfb71k3ws=p@&'
+SECRET_KEY = os.environ.get('PP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
